82a99e7ebae688ae71dd692e92fef1be0d0c3db8
ilario-moreni <ilario2001.mor@gmail.com>
2022-12-20 15:01:11 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-alert("i'm linked")
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            todoList: [
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'Lavare i piatti',
+                    done: true
+                },
+                {
+                    text: 'Portare a passeggio il cane',
+                    done: true
+                },
+                {
+                    text: 'Organizzare capodanno',
+                    done: false
+                },
+                {
+                    text: 'Andare in palestra',
+                    done: false
+                }
+            ]
+        }
+    }
+}).mount('#app')
